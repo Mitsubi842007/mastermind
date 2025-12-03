d5e1d9b1d27e9ca4aa8ddfaa0637cc938c767ff1
Mitsubi <agp.842007@gmail.com>
2025-12-03 11:50:07 +0100

    array list,array and random
    array list and an normal array + random are added so now the code is random

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,90 +1,97 @@
 package mastermind;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Mastermind {
 
 	public static void main(String[] args) {
 
-		System.out.println("welkom bij mastermind" + "         gebruik vier kleuren");
-
+		System.out.println("welcome to mastermind" + "use four colors");
+		Scanner sc = new Scanner(System.in);
 //rood, blauw , groen , oranje , zwart , wit , paars, geel
 //code kraker
-		
-		String green = "green";
-		String red = "red";
-		String yellow = "yellow";
-		String blue = "blue";
-		String black = "black";
-		String white = "white";
-		String purple = "purple";
-		String orange = "orange";
+		ArrayList<String> color = new ArrayList<String>();
+		color.add("green");
+		color.add("blue");
+		color.add("red");
+		color.add("orange");
+		color.add("purple");
+		color.add("yellow");
+		color.add("black");
+		color.add("white");
 
-		 //make code
-		String code1 = green;
-		String code2 = blue;
-		String code3 = blue;
-		String code4 = orange;
+		// make code
 
-		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
 
+		String code1 = color.get(rand.nextInt(6));
+		String code2 = color.get(rand.nextInt(6));
+		String code3 = color.get(rand.nextInt(6));
+		String code4 = color.get(rand.nextInt(6));
 
 		boolean Won = false;
-		for (int i = 10; i > 0; i--) {
-			
-			System.out.println("you can geuss " + i + " times");
-			
+		for (int guess = 10; guess > 0; guess--) {
+
+			System.out.println("you can guess " + guess + " times");
+			System.out.println("- green -blue -red -orange -purple -yellow");
 			// code Guess
-			String guess1 = sc.next();
-			String guess2 = sc.next();
-			String guess3 = sc.next();
-			String guess4 = sc.next();
+
+			String[] guesses = new String[4];
+
+			for (int i = 0; i < guesses.length; i++) {
+				guesses[i] = sc.next();
+			}
 
 			// code check
+			String[] checks = { "..", "..", "..", ".." };
+			for (int i = 0; i < checks.length; i++) {
 
-			String check1 = "... ";
-			String check2 = "... ";
-			String check3 = "... ";
-			String check4 = "... ";
-
-			if (guess1.equalsIgnoreCase(code1)) {
-				check1 = black;
-
-			} else if (guess1.equalsIgnoreCase(code2) || guess1.equalsIgnoreCase(code3)
-					|| guess1.equalsIgnoreCase(code4)) {
-				check1 = white;
 			}
-			if (guess2.equalsIgnoreCase(code2)) {
-				check2 = black;
-			} else if (guess1.equalsIgnoreCase(code1) || guess1.equalsIgnoreCase(code3)
-					|| guess1.equalsIgnoreCase(code4)) {
-				check2 = white;
+			// guess 1
+			if (guesses[0].equalsIgnoreCase(code1)) {
+				checks[0] = color.get(6);
+			} else if (guesses[0].equalsIgnoreCase(code2) || guesses[0].equalsIgnoreCase(code3)
+					|| guesses[0].equalsIgnoreCase(code4)) {
+				checks[0] = color.get(7);
 			}
-			if (guess3.equalsIgnoreCase(code3)) {
-				check3 = black;
-			} else if (guess1.equalsIgnoreCase(code2) || guess1.equalsIgnoreCase(code1)
-					|| guess1.equalsIgnoreCase(code4)) {
-				check3 = white;
+			// guess 2
+			if (guesses[1].equalsIgnoreCase(code2)) {
+				checks[1] = color.get(6);
+			} else if (guesses[1].equalsIgnoreCase(code1) || guesses[1].equalsIgnoreCase(code3)
+					|| guesses[1].equalsIgnoreCase(code4)) {
+				checks[1] = color.get(7);
 			}
-			if (guess4.equalsIgnoreCase(code4)) {
-				check4 = black;
-			} else if (guess1.equalsIgnoreCase(code2) || guess1.equalsIgnoreCase(code3)
-					|| guess1.equalsIgnoreCase(code1)) {
-				check4 = white;
+			// guess 3
+			if (guesses[2].equalsIgnoreCase(code3)) {
+				checks[2] = color.get(6);
+			} else if (guesses[2].equalsIgnoreCase(code2) || guesses[2].equalsIgnoreCase(code1)
+					|| guesses[2].equalsIgnoreCase(code4)) {
+				checks[2] = color.get(7);
+			}
+			// guess 4
+			if (guesses[3].equalsIgnoreCase(code4)) {
+				checks[3] = color.get(6);
+			} else if (guesses[3].equalsIgnoreCase(code2) || guesses[3].equalsIgnoreCase(code3)
+					|| guesses[3].equalsIgnoreCase(code1)) {
+				checks[3] = color.get(7);
 			}
 
-			System.out.println("check: " + guess1 + " " + guess2 + " " + guess3 + " " + guess4 + " : " + check1 + " "
-					+ check2 + " " + check3 + " " + check4);
+			System.out.println("check: " + guesses[0] + " " + guesses[1] + " " + guesses[2] + " " + guesses[3] + " : "
+					+ checks[0] + " " + checks[1] + " " + checks[2] + " " + checks[3]);
 
-			if (check1.equals(black) && check2.equals(black) && check3.equals(black) && check4.equals(black)) {
+			if (checks[0].equals(color.get(6)) && checks[1].equals(color.get(6)) && checks[2].equals(color.get(6))
+					&& checks[3].equals(color.get(6))) {
 				System.out.println("you won the mastermind game");
 				Won = true;
-				i = 0;
+				guess = 0;
 			}
 
 		}
-		if(Won == false) {
+		if (Won == false) {
 			System.out.println("You lost");
+			sc.close();
 		}
 
 	}
